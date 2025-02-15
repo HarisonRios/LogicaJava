20f73a8bce78bd9541afc8d080d1321aba4956cb
Harison Rios de Miranda <hharison562@gmail.com>
2025-02-15 02:19:39 -0300

    novos exercicios

---
+++ Lista 1 - Nivelamento/ex1.java
@@ -0,0 +1,40 @@
+package school.sptech;
+
+import java.util.Scanner;
+
+public class ex1 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int filho0a4 = 0;
+    int filho4a16 = 0;
+    int filho17a18 = 0;
+
+    double valor_0a3 = 25.12;
+    double valor_4a16 = 195.88;
+    double valor_17a18 = 12.44;
+
+    System.out.print("Quantos filhos de 0 a 3 anos possui: ");
+    filho0a4 = scanner.nextInt();
+
+    System.out.print("Quantos filhos de 4 a 16 anos possui: ");
+    filho4a16 = scanner.nextInt();
+
+    System.out.print("Quantos filhos de 17 a 18 anos possui: ");
+    filho17a18 = scanner.nextInt();
+
+    double totalBolsa0a3 = filho0a4 * valor_0a3;
+    double totalBolsa4a16 = filho4a16 * valor_4a16;
+    double totalBolsa17a18 = filho17a18 * valor_17a18;
+
+    if (filho0a4 < 3) {
+      System.out.println("Você tem um total de " + filho0a4 + " filhos e vai receber " + totalBolsa0a3 + " de bolsa.");
+    } else if (filho4a16 > 4 && filho4a16 < 16) {
+      System.out.println("Voce tem um total de " + filho4a16 + " filhos e vai receber " + totalBolsa4a16 + " de bolsa.");
+    } else if (filho17a18 >= 17) {
+      System.out.println("Voce tem um total de  " + filho17a18 + " filhos e vai receber "  + totalBolsa17a18 + " de bolsa.");
+    }
+    scanner.close();
+
+  }
+}
---
+++ Lista 1 - Nivelamento/ex2.java
@@ -0,0 +1,38 @@
+package school.sptech;
+
+import java.util.Scanner;
+
+public class ex2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int tempoAquecimento = 0;
+        int tempoAerobico = 0;
+        int tempoMusculacao = 0;
+
+        double aquecimento = 12;
+        double aerobico = 20;
+        double musculacao = 25;
+
+
+        System.out.println("Digite o tempo de aquecimento em minutos: ");
+        tempoAquecimento = scanner.nextInt();
+
+        System.out.println("Digite o tempo de exercício aerobico em minutos: ");
+        tempoAerobico = scanner.nextInt();
+
+        System.out.println("Digite o tempo de exercício musculacao em minutos: ");
+        tempoMusculacao = scanner.nextInt();
+
+
+        double totalExercicios = tempoAquecimento + tempoAerobico + tempoMusculacao;
+        double totalCalorias = (tempoAquecimento * aquecimento) + (tempoAerobico * aerobico) + (tempoMusculacao * musculacao);
+
+
+        System.out.println("Olá, Jorge. Você fez um total de " + totalExercicios + " minutos de exercícios e perdeu cerca de " + totalCalorias + " calorias.\n");
+
+
+        scanner.close();
+
+    }
+}
